6e666f36ae627782c9d1f060dbb7a144da27a07d
xiquexique708 <jpaulo.silvagm@gmail.com>
2026-04-19 22:24:37 -0300

    Add files via upload

---
+++ js/modal_whatsapp.js
@@ -0,0 +1,24 @@
+$(document).ready(function(){
+	$("#modal_whatsapp").hide();
+	$("#corpo_modal").hide();
+	
+	$(".whatsapp").click(function() {
+		$("#modal_whatsapp").fadeIn(function() {
+			$("#corpo_modal").show(function() {
+				$("#corpo_modal").animate({
+					top: "100px"
+				}, 300);
+			})
+		});
+	});
+	
+	$(".fechar").click(function() {
+		$("#modal_whatsapp").fadeOut(function() {
+			$("#corpo_modal").hide(function() {
+				$("#corpo_modal").animate({
+					top: "-100%"
+				});
+			});
+		});
+	});
+});
